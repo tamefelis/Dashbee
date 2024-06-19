17b807e7b1bbf39e2a3afa420d984bcdbc5d61ab
tamefelis <126751258+tamefelis@users.noreply.github.com>
2024-06-19 17:00:35 +0800

    Update ABLEdashbee.py

--- ABLEdashbee.py
+++ ABLEdashbee.py
@@ -415,7 +415,7 @@ def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.save()
+    writer.close()
     processed_data = output.getvalue()
     return processed_data
 
